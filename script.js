a42e61f92d7cdccd9f940495e9680803dfc00fd4
martinwong202 <martin.wong202@gmail.com>
2020-09-19 21:14:06 -0700

    Improve Backend Jquery

--- script.js
+++ script.js
@@ -9,3 +9,34 @@ $("#VSubmit").click(function(){
     $("#VMessages").append("<div><h2 id='TextDisplay'>"+searched+"</h2></div><hr>");
     $("#TextDisplay").css({ 'color': 'orange'});
 });
+$("#SBSubmit").click(function(){
+    var searched = $("#SBPost").val();
+    $("#SBMessages").append("<div><h2 id='TextDisplay'>"+searched+"</h2></div><hr>");
+    $("#TextDisplay").css({ 'color': 'orange'});
+});
+$("#AUSubmit").click(function(){
+    var searched = $("#AUPost").val();
+    $("#AUMessages").append("<div><h2 id='TextDisplay'>"+searched+"</h2></div><hr>");
+    $("#TextDisplay").css({ 'color': 'orange'});
+});
+$("#FGSubmit").click(function(){
+    var searched = $("#FGPost").val();
+    $("#FGMessages").append("<div><h2 id='TextDisplay'>"+searched+"</h2></div><hr>");
+    $("#TextDisplay").css({ 'color': 'orange'});
+});
+$("#VUpload").click(function(){
+    var uploaded = $("#VPost").val();
+    $("#VMessages").append("<div><img id='Uploadimg' src='"+uploaded+"'></div><hr>");
+});
+$("#SBUpload").click(function(){
+    var uploaded = $("#SBPost").val();
+    $("#SBMessages").append("<div><img id='Uploadimg' src='"+uploaded+"'></div><hr>");
+});
+$("#AUUpload").click(function(){
+    var uploaded = $("#AUPost").val();
+    $("#AUMessages").append("<div><img id='Uploadimg' src='"+uploaded+"'></div><hr>");
+});
+$("#FGUpload").click(function(){
+    var uploaded = $("#FGPost").val();
+    $("#FGMessages").append("<div><img id='Uploadimg' src='"+uploaded+"'></div><hr>");
+});
