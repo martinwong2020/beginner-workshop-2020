f801e635f4db5e12c4d5accba076f66112ff2514
martinwong202 <martin.wong202@gmail.com>
2020-09-19 19:33:51 -0700

    Developed the Html Javascript and CSS of the other pages

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 $(".login").click(function(){
-    return alert("it works");
-    console.log("true");
+    return alert("In working progress");
+    return console.log("true");
 });
 
 
-
+$("#VSubmit").click(function(){
+    var searched = $("#VPost").val();
+    $("#VMessages").append("<div><h2 id='TextDisplay'>"+searched+"</h2></div><hr>");
+    $("#TextDisplay").css({ 'color': 'orange'});
+});
